17a24f8ba3951c806dbcd91dee5deb4dfa2e0c10
Valerii Viun <2455521@gmail.com>
2023-04-05 21:38:25 +0300

    task1_finish

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,43 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import { galleryItems } from "./gallery-items.js";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Change code below this line
 
+//створюємо слоти із зображенням з масива у строку
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+function galleryItemCreate(galleryItems) {
+    return galleryItems
+      .map(({ preview, original, description }) => {
+        return `<li class="gallery__item">
+              <a class="gallery__link" href="${original}"><img
+              class="gallery__image"
+              src="${preview}"
+              data-source="${original}"
+              alt="${description}"
+            />
+          </a>
+        </li>`;
+      })
+      .join("");
+  }
+  const items = galleryItemCreate(galleryItems); //створення посилання на строку з зображенням
+  console.log(galleryItemCreate(galleryItems)); //перевірка створення div з class="gallery__item"
+  
+  //додаємо слоти з зображенням до розмітки
+  
+  const gallery = document.querySelector(".gallery"); // DOM для gallery
+  
+  gallery.insertAdjacentHTML("beforeend", items); // додаємо до HTML розмітки зображення
+
+  const lightbox = new SimpleLightbox(".gallery a", {
+
+    captionsData: "alt",
+    animationSpeed: 250,
+    maxZoom: 20
+  });
 
 
-console.log("galleryItems");
+console.log(galleryItems);
