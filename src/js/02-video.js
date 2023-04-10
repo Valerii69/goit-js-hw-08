8f41b4b34a616e559713e81dc23cb494b1de3e35
Valerii Viun <2455521@gmail.com>
2023-04-10 15:48:04 +0300

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,16 +1,12 @@
 import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
-
-
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
-
 const currentTime = 'videoplayer-current-time';
 const localCurrentTime = localStorage.getItem(currentTime);
-
 const onPlay = function (timeUpdate) {
-  console.log(timeUpdate);
+  // console.log(timeUpdate);
   localStorage.setItem(currentTime, timeUpdate.seconds);
 };
 
@@ -21,9 +17,9 @@ player
   .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
-      case 'RangeError': break;
-      default: break;
+      case 'RangeError':
+        break;
+      default:
+        break;
     }
   });
-
-
